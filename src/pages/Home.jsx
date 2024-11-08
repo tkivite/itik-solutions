80363ab47d85e60dc4712039b0758c70c4c5b7be
Brian <brianhilsden@gmail.com>
2024-11-08 07:00:28 +0300

    added company logo to head

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -82,7 +82,7 @@ const Home = () => {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              transition={{ duration: 0.5 }}
+              transition={{ duration: 0.8 }}
             />
           </AnimatePresence>
         </div>
