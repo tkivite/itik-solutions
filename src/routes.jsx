2641b31ada4175687e45fa85c1515df36bd18f7e
Brian <brianhilsden@gmail.com>
2024-11-08 06:51:46 +0300

    added vercel configuration

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,26 +1,25 @@
 import App from "./App";
 import About from "./pages/About";
 import Contact from "./pages/Contacts";
- import ErrorPage from "./pages/ErrorPage"; 
+import ErrorPage from "./pages/ErrorPage";
 
 import Home from "./pages/Home";
 import Mentorship from "./pages/Mentorship";
 import ProductsServices from "./pages/ProductsServices";
 
-
 const routes = [
-    { path: "/", 
-    element: <App /> ,
-    errorElement:<ErrorPage/>,
-        children:[
-            {path:"/",element:<Home/>},
-            {path:"/services",element:<ProductsServices/>},
-            {path:"/mentorship",element:<Mentorship/>},
-            {path:'/about',element:<About/>},
-            {path:'/contact',element:<Contact/>}
-          
-
-        ]
-}];
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/services", element: <ProductsServices /> },
+      { path: "/mentorship", element: <Mentorship /> },
+      { path: "/about", element: <About /> },
+      { path: "/contact", element: <Contact /> },
+    ],
+  },
+];
 
 export { routes };
